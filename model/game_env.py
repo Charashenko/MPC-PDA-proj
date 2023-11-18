d86bf4dbc03001536d1fbe92f065570a720719a6
Charashenko <charashenko@gmail.com>
2023-11-18 21:35:51 +0100

    add game environment for tensorflow

---
+++ model/game_env.py
@@ -0,0 +1,108 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import tensorflow as tf
+import numpy as np
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
+from tf_agents.trajectories import time_step as ts
+
+OBSERVATION_SPEC_SIZE = 14
+# DEBUG
+OPPS = 10
+# -----
+
+
+class GameEnv(py_environment.PyEnvironment):
+    def __init__(
+        self,
+        state_getter: callable,
+        num_of_opponents_getter: callable,
+        on_robot_death_getter: callable,
+    ):
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(5,),
+            dtype=np.float64,
+            minimum=-1,
+            maximum=1,
+            name="action",
+        )
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(OBSERVATION_SPEC_SIZE,),
+            dtype=np.float64,
+            minimum=-1,
+            maximum=1,
+            name="observation",
+        )
+        self._state = np.zeros([OBSERVATION_SPEC_SIZE])
+        self._on_robot_death_getter = on_robot_death_getter
+        self._episode_ended = False
+        self._state_getter = state_getter
+        self._num_of_opponents_getter = num_of_opponents_getter
+        self._init_num_of_opponents = num_of_opponents_getter()
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def _reset(self):
+        self._state = np.zeros([OBSERVATION_SPEC_SIZE])
+        self._episode_ended = False
+        return ts.restart(self._state)
+
+    def _step(self, action):
+        # DEBUG
+        global OPPS
+        # -----
+        if self._episode_ended:
+            return self.reset()
+
+        self._state = self._state_getter()
+
+        if self._num_of_opponents_getter() == 0 or self._on_robot_death_getter():
+            self._episode_ended = True
+        else:
+            # TODO feed to RL network here
+            pass
+
+        if self._episode_ended:
+            reward = self._init_num_of_opponents - self._num_of_opponents_getter()
+            return ts.termination(self._state, reward)
+        else:
+            # DEBUG
+            OPPS -= 1
+            # -----
+            return ts.transition(self._state, reward=0.0, discount=1.0)
+
+
+# DEBUG
+def state_getter():
+    arr = np.zeros([OBSERVATION_SPEC_SIZE])
+    return arr
+
+
+# DEBUG
+def num_of_opponents_getter():
+    global OPPS
+    return OPPS
+
+
+# DEBUG
+def on_robot_death_getter():
+    return False
+
+
+# DEBUG
+environment = GameEnv(state_getter, num_of_opponents_getter, on_robot_death_getter)
+utils.validate_py_environment(environment, episodes=5)
+# -----
--- model/utils.py
+++ model/utils.py
@@ -3,7 +3,7 @@ class DataProcessor:
         self.map_size = map_size
 
     def __process_heading(self, data):
-        # Limit heading to 0-360 degrees and map it to -1 <-> 1 = -180 <-> 180
+        # Limit heading to 0-360 degrees and map -180 <-> 180 to -1 <-> 1
         negative = False
         if data < 0:
             negative = True
@@ -18,36 +18,43 @@ class DataProcessor:
         return data
 
     def pos_x(self, data):
-        # Map 0 <-> map width = -1 <-> 1
+        # input
+        # Map 0 <-> map width to -1 <-> 1
         return (data / self.map_size.width() - 0.5) * 2
 
     def pos_y(self, data):
-        # Map 0 <-> map width = -1 <-> 1
+        # input
+        # Map 0 <-> map width to -1 <-> 1
         return (data / self.map_size.height() - 0.5) * 2
 
     def health(self, data):
-        # Max health is 100, 0 <-> 1 = 0 <-> 100
-        return data / 100
+        # input
+        # Max health is 100, map 0 <-> 100 to -1 <-> 1
+        return (data / 100 - 0.5) * 2
 
     def gun_heading(self, data):
+        # input
         return self.__process_heading(data)
 
     def tank_heading(self, data):
+        # input
         return self.__process_heading(data)
 
     def radar_heading(self, data):
+        # input
         return self.__process_heading(data)
 
     def move(self, data):
         # TODO do calculation based on map size using trigonometry
+        # Map -1 <-> 1 to 0 <-> current max
         return data
 
     def turn(self, data):
-        # Map -1 <-> 1 = -180 <-> 180
+        # Map -1 <-> 1 to -180 <-> 180
         return data * 180
 
     def fire(self, data):
-        # Map -1 <-> 1 = 1 <-> 10
+        # Map -1 <-> 1 to 1 <-> 10
         if data < 0:
             data *= -1
             calc = 5 - int(5 * data)
@@ -57,5 +64,5 @@ class DataProcessor:
         return int(5 + 5 * data)
 
     def radar_turn(self, data):
-        # Map -1 <-> 1 = -180 <-> 180
+        # Map -1 <-> 1 to -180 <-> 180
         return data * 180
